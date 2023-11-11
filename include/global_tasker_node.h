3b8aef4eba8b51b02bb05314e7b4ff8c55345637
m5m14 <20xim04@gmail.com>
2023-11-11 18:04:00 +0200

    fix

--- include/global_tasker_node.h
+++ include/global_tasker_node.h
@@ -1,12 +1,11 @@
 #pragma once
-#define NAME_CLIENT "global_tasker_node"
-#define NAME_SERVER "global_tasker_node"
 #include"web_server.h"
 #include <iostream>
 
 class global_tasker_node {
 public:
   global_tasker_node() {
+    web.set_name_server("global_tasker_node");
     web.add_conn_server("127.0.0.1:3000");
     web.start_server(3001);
   }
